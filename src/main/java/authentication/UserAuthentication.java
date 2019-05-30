1b72739bedc91764d7ebc3b61965bd714f8f9c40
lkhoudi <lkhoudi@cosium.com>
2019-05-30 17:28:08 +0200

    authentication ans sign up

--- src/main/java/authentication/UserAuthentication.java
+++ src/main/java/authentication/UserAuthentication.java
@@ -1,16 +1,44 @@
 package authentication;
 
 import entities.User;
+import io.javalin.Context;
+import io.javalin.Javalin;
 import managers.UserDbManager;
 
 public class UserAuthentication {
 
-    public Boolean authenticateUser(String email, String password){
+    public void authenticateUser(String email, String password){
         User user = new UserDbManager().getUserByEmail(email);
-        return user.getPassword().equals(password);
+        boolean isAuthenticated = user.getPassword().equals(password);
+        if(isAuthenticated){
+            System.out.println("ok");
+        }
     }
 
     public User signUpUser(User user){
         return new UserDbManager().saveUser(user);
     }
+
+    public Javalin startListener(){
+        return Javalin.create()
+                .enableStaticFiles("/public")
+                .start(7777);
+    }
+
+    public void login(Context context, Javalin app){
+
+        app.post("/login", ctx -> {
+            String email = ctx.formParam("email");
+            String password = ctx.formParam("password");
+            this.authenticateUser(email, password);
+            ctx.html("You're logged in");
+        });
+    }
+
+    public static void main(String [] s){
+        UserAuthentication userAuthentication = new UserAuthentication();
+        userAuthentication.startListener();
+    }
+
+
 }
