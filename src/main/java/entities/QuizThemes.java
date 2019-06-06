5f4eafa16c872248c994e4d1e648c749cd4ebe4c
lkhoudi <lkhoudi@cosium.com>
2019-06-06 11:20:03 +0200

    Merge branch 'master' of https://github.com/lkhoudi/AppliReseau
    # Conflicts:
    #	src/main/java/PartieClient/Client.java

---
+++ src/main/java/entities/QuizThemes.java
@@ -0,0 +1,23 @@
+package com.quiz.demoQuiz.entity;
+
+public enum QuizThemes {
+
+
+    ANIMALS("Animals.json"),
+    BAKERY("Bakery.json"),
+    CULTURE("General Culture.json"),
+    OCEAN("meditarranee.json"),
+    MUSIC("Music.json"),
+    COUNTRIES("worldCountries.json");
+
+    private final String value;
+
+    QuizThemes(String value) {
+        this.value = value;
+    }
+
+
+    public String getValue() {
+        return value;
+    }
+}
